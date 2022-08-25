3a1d380b301345e509a746523c3106d4fafa3c98
zoharsamra <103178629+zoharsamra@users.noreply.github.com>
2022-08-25 17:43:18 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,2 +1,15 @@
-def funci():
-    pass
+import numpy as np
+
+def generate_key():
+    return np.random.randint(0, 1000)
+
+def generate_matrix(shape):
+    return np.random.randint(low=0,high=255,size=shape)
+
+def bird_choise(mat, field_shape, id):
+    np.random.seed(id)
+    noise = np.random.rand(*field_shape)
+    noise = noise * mat
+    idx = np.unravel_index(np.argmax(noise, axis=None), noise.shape)
+    return idx
+
